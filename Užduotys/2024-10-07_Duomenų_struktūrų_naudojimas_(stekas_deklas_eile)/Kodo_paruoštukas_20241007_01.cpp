6fffc2dc10a0feb649d2d8fbbef231add2c2d7cb
Andrej <54258807+agorbatniov@users.noreply.github.com>
2024-10-07 09:58:26 +0300

    C++ paruoštuko pakeitimas

--- Užduotys/2024-10-07_Duomenų_struktūrų_naudojimas_(stekas_deklas_eile)/Kodo_paruoštukas_20241007_01.cpp
+++ Užduotys/2024-10-07_Duomenų_struktūrų_naudojimas_(stekas_deklas_eile)/Kodo_paruoštukas_20241007_01.cpp
@@ -2,23 +2,27 @@
 #include <queue>
 #include <stack>
 #include <deque>
+#include <string>  // For using std::string
+
+// Using standard library types without std:: prefix
+using namespace std;
 
 class BankTransactions {
 public:
-    std::queue<std::string> queue;  // Naudojame kaip eilę
-    std::stack<std::string> stack;  // Naudojame kaip steką
-    std::deque<std::string> deq;    // Naudojame kaip deklą
+    queue<string> transactionQueue;  // Naudojame kaip eilę
+    stack<string> transactionStack;  // Naudojame kaip steką
+    deque<string> transactionDeque;  // Naudojame kaip deklą
 
     // Eilės funkcijos
-    void enqueue_transaction(const std::string& transaction) {
+    void enqueue_transaction(const string& transaction) {
         // TODO: įdėti elementą į eilę
-        // queue.
+        // transactionQueue.
     }
 
-    std::string dequeue_transaction() {
-        if (!queue.empty()) {
+    string dequeue_transaction() {
+        if (!transactionQueue.empty()) {
             // TODO: paimti pirmą elementą iš eilės
-            // std::string front = queue.
+            // string front =
             //
             return front;
         }
@@ -26,46 +30,46 @@ public:
     }
 
     // Steko funkcijos
-    void push_transaction(const std::string& transaction) {
-        // TODO: įdėti elementą į steką
-        // stack.
+    void push_transaction(const string& transaction) {
+        // įdėti elementą į steką
+        //transactionStack.
     }
 
-    std::string pop_transaction() {
-        if (!stack.empty()) {
-            // TODO: paimti paskutinį elementą iš steko 
-            // std::string top = 
-            // 
+    string pop_transaction() {
+        if (!transactionStack.empty()) {
+            // TODO: paimti pirmą elementą iš steko
+            // string top = transactionStack.top();
+            //
             return top;
         }
         return "Stack is empty";
     }
 
     // Deklo funkcijos
-    void add_front_transaction(const std::string& transaction) {
-        // TODO: įdėti elementą į priekį
-        // deq.
+    void add_front_transaction(const string& transaction) {
+        // TODO: įdėti elementą į dekla iš priekio
+        // transactionDeque.
     }
 
-    void add_rear_transaction(const std::string& transaction) {
-        // TODO: įdėti elementą į priekį
-        // deq.
+    void add_rear_transaction(const string& transaction) {
+        // TODO: įdėti elementą į dekla iš galo
+        // transactionDeque.
     }
 
-    std::string remove_front_transaction() {
-        if (!deq.empty()) {
+    string remove_front_transaction() {
+        if (!transactionDeque.empty()) {
             // TODO: paimti pirmą elementą iš deklo
-            // std::string front = 
+            // string front = 
             //
             return front;
         }
         return "Deque is empty";
     }
 
-    std::string remove_rear_transaction() {
-        if (!deq.empty()) {
+    string remove_rear_transaction() {
+        if (!transactionDeque.empty()) {
             // TODO: paimti paskutinį elementą iš deklo
-            // std::string back = 
+            // string back = 
             // 
             return back;
         }
@@ -79,18 +83,18 @@ int main() {
     // Eilės operacijos
     bt.enqueue_transaction("Client 1");
     bt.enqueue_transaction("Client 2");
-    std::cout << "Eilėje aptarnaujamas: " << bt.dequeue_transaction() << std::endl;  // Client 1
+    cout << "Eilėje aptarnaujamas: " << bt.dequeue_transaction() << endl;  // Client 1
 
     // Steko operacijos
     bt.push_transaction("Client 1");
     bt.push_transaction("Client 2");
-    std::cout << "Steke aptarnaujamas: " << bt.pop_transaction() << std::endl;  // Client 2
+    cout << "Steke aptarnaujamas: " << bt.pop_transaction() << endl;  // Client 2
 
     // Deklo operacijos
     bt.add_rear_transaction("Client 1");
     bt.add_front_transaction("VIP Client");
-    std::cout << "Dekle aptarnaujamas iš priekio: " << bt.remove_front_transaction() << std::endl;  // VIP Client
-    std::cout << "Dekle aptarnaujamas iš galo: " << bt.remove_rear_transaction() << std::endl;  // Client 1
+    cout << "Dekle aptarnaujamas iš priekio: " << bt.remove_front_transaction() << endl;  // VIP Client
+    cout << "Dekle aptarnaujamas iš galo: " << bt.remove_rear_transaction() << endl;  // Client 1
 
     return 0;
 }
