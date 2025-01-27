0ea95470549dd1b92118d9c470039b46364aa0d2
Andrej <54258807+agorbatniov@users.noreply.github.com>
2025-01-27 06:58:53 +0200

    Duomenų tyrybos, programavimo ir saugaus elgesio pradmenys

---
+++ Duomenų tyrybos, programavimo ir saugaus elgesio pradmenys/01-Informacijos_(rezultatų)_pateikimas/filtravimas_template.py
@@ -0,0 +1,8 @@
+import pandas as pd
+
+# Sukuriamas duomenų rinkinys
+data = {
+    'Vardas': ['Jonas', 'Asta', 'Lukas', 'Rūta'],
+    'Amžius': [25, 22, 24, 23],
+    'Pajamos': [500, 600, 700, 550]
+}
